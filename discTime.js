0c00c5291761fb25b0b18ca6659db3bf16da8761
muslik98 <muslikhudinm@gmail.com>
2023-08-15 19:15:27 +0700

    add discount Time

---
+++ discTime.js
@@ -0,0 +1,29 @@
+const discTime = document.querySelector("#discount p:nth-of-type(2)");
+let countDownDate = new Date("Aug 16, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+  // Get today's date and time
+  let now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  let distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  discTime.innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    discTime.innerHTML = "EXPIRED";
+    console.log(x);
+  }
+}, 1000);
